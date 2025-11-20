c2a532e4937bb40ce856e65bdc5c71db85bda203
mdjaveedkhan <mdjaveed1802@gmail.com>
2025-11-20 21:14:10 +0530

    Second program

--- day1.java
+++ day1.java
@@ -1,5 +1,7 @@
 public class day1{
     public static void main(String args[]){
        System.out.println("MD JAVEED KHAN");
+        System.out.println("My name is "+args[0]+" "+args[1]+" "+args[2]);
+        System.out.println("I am "+args[3]+" Years old");
     }
 }
---
+++ day2.java
@@ -0,0 +1,6 @@
+public class day2{
+    public static  void main(String args[]){
+        System.out.println("MD JAVEED KHAN");
+    }
+
+}
