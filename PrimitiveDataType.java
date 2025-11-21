5e47f1f2863fb32c829c3ec6cc7ad395113c8a1d
mdjaveedkhan <mdjaveed1802@gmail.com>
2025-11-21 19:45:54 +0530

    Primitive DataType

---
+++ PrimitiveDataType.java
@@ -0,0 +1,35 @@
+public class PrimitiveDataType{
+    public static void main(String[] args) {
+        // Integer data type
+        int myInt = 10;
+        System.out.println("Integer value: " + myInt);
+
+        // Floating-point data type
+        float myFloat = 10.5f;
+        System.out.println("Float value: " + myFloat);
+
+        // Character data type
+        char myChar = 'A';
+        System.out.println("Character value: " + myChar);
+
+        // Boolean data type
+        boolean myBool = false;
+        System.out.println("Boolean value: " + myBool);
+
+        // Double data type
+        double myDouble = 20.99;
+        System.out.println("Double value: " + myDouble);
+
+        // Long data type
+        long myLong = 150000L;
+        System.out.println("Long value: " + myLong);
+
+        // Short data type
+        short myShort = 1000;
+        System.out.println("Short value: " + myShort);
+
+        // Byte data type
+        byte myByte = 100;
+        System.out.println("Byte value: " + myByte);
+    }
+}
